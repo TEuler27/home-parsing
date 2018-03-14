0ef0c2bffb28ae439fbf3284e890b13510b7c664
edo1998 <edo.iscrizioni@rinonline.com>
2018-03-14 16:58:30 +0100

    Aggiunto getLocalita

--- geografiaimmobiliare.py
+++ geografiaimmobiliare.py
@@ -5,7 +5,8 @@ import json
 class GeografiaImmobiliare(object):
 
 	def __init__(self):
-		self.geografia = {"Valle D'Aosta" : {},"Lombardia" : {},"Piemonte" : {},"Liguria" : {},"Trentino Alto-Adige" : {},"Veneto" : {},"Friuli Venezia-Giulia" : {},"Emilia Romagna" : {},"Toscana" : {},"Marche" : {},"Lazio" : {},"Umbria" : {},"Abruzzo" : {},"Molise" : {},"Basilicata" : {},"Puglia" : {},"Campania" : {},"Calabria" : {},"Sicilia" : {},"Sardegna" : {}}
+		#self.geografia = {"Valle D'Aosta" : {},"Lombardia" : {},"Piemonte" : {},"Liguria" : {},"Trentino Alto-Adige" : {},"Veneto" : {},"Friuli Venezia-Giulia" : {},"Emilia Romagna" : {},"Toscana" : {},"Marche" : {},"Lazio" : {},"Umbria" : {},"Abruzzo" : {},"Molise" : {},"Basilicata" : {},"Puglia" : {},"Campania" : {},"Calabria" : {},"Sicilia" : {},"Sardegna" : {}}
+		self.geografia = {"Lombardia": {}}
 
 	def getProvince(self):
 		for regione in self.geografia:
@@ -23,7 +24,7 @@ class GeografiaImmobiliare(object):
 				req =  request.Request("https://www.immobiliare.it/services/geography/getGeography.php?action=getComuniProvincia&idProvincia="+sigla, headers=header)
 				json_data = json.loads(request.urlopen(req).read().decode("utf8"))
 				for comune in json_data["result"]:
-					self.geografia[regione][provincia][comune["nome"]] = {"idComune": comune["idComune"], "conZone": comune["conZone"], "conLocalita": comune["conLocalita"]}
+					self.geografia[regione][provincia][comune["nome"]] = {"idComune": comune["idComune"], "conZone": comune["conZone"], "conLocalita": comune["conLocalita"], "keyurl": comune["keyurl"]}
 
 	def getZone(self):
 		for regione in self.geografia:
@@ -38,9 +39,21 @@ class GeografiaImmobiliare(object):
 						for idZona in json_data["result"]:
 							self.geografia[regione][provincia][comune]["Zone"] += [{"Nome" : json_data["result"][idZona]['macrozona_nome_sn'], "keyurl": json_data["result"][idZona]['macrozona_keyurl']}]
 
+	def getLocalita(self):
+		for regione in self.geografia:
+			for provincia in self.geografia[regione]:
+				for comune in self.geografia[regione][provincia]:
+					if self.geografia[regione][provincia][comune]["conLocalita"]:
+						pagina = pq(request.urlopen("https://www.immobiliare.it/vendita-case/"+self.geografia[regione][provincia][comune]["keyurl"].lower().replace("_","-")).read().decode("utf8"))
+						self.geografia[regione][provincia][comune]["Localita"] = []
+						for localita in pagina(".breadcrumb-list").items("a"):
+							self.geografia[regione][provincia][comune]["Localita"] += [localita.text()]
+							print(localita.text())
+
 	def getGeografia(self):
 		self.getProvince()
 		self.getComuni()
 		self.getZone()
+		self.getLocalita()
 		fh = open("zone.txt","w")
 		fh.write(json.dumps(self.geografia))
