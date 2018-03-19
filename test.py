9db7abb968d8474aebb82ed5561f99b29da89d33
zozzo98 <ale.zonato@hotmail.it>
2018-03-19 18:43:39 +0100

    pronti alla fusioneeeeeee

--- test.py
+++
@@ -1,8 +0,0 @@
-from HomeParsing import *
-from immobiliare import *
-
-HP = HomeParsing()
-#HP.ExtractAnnunci("https://www.immobiliare.it/vendita-case/monza/?criterio=rilevanza&pag=110",selettore,funzione,next)
-lista = HP.ExtractAnnunci("https://www.immobiliare.it/vendita-case/monza/?criterio=rilevanza",selettore,funzione,next)
-for link in lista:
-    HP.ExtractData(link,"prova.csv",selettori,funzioni)
