92bd69862756f76fd833b172870567da109a13e4
edo1998 <edo.iscrizioni@rinonline.com>
2018-03-27 18:45:14 +0200

    Pre-merge

--- test.py
+++
@@ -1,6 +0,0 @@
-from HomeParsing import *
-from idealista import *
-
-HP = HomeParsing(new = 1)
-ID = Idealista("A")
-HP.ExtractData("https://www.idealista.it/immobile/13347140/","prova.csv",ID.funzioni)
