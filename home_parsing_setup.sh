52293a54607d4f4fe6a732b4b3d8a2b76615d17e
edo1998 <edo.iscrizioni@rinonline.com>
2018-04-15 22:36:46 +0200

    aggiunto install tkinter

--- home_parsing_setup.sh
+++ home_parsing_setup.sh
@@ -1,5 +1,6 @@
 sh /Applications/Python\ 3.6/Install\ Certificates.command
 pip3 install pyquery
 pip3 install requests
+pip3 install tkinter
 cd /Applications
 git clone https://www.github.com/edo1998/home-parsing
