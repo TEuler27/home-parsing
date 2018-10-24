9e2988c19c7535e495ea915e4611e7fcdb1cc69b
edo1998 <edo.iscrizioni@rinonline.com>
2018-10-24 18:16:53 +0200

    Bug fixes

--- home_parsing_update.bat
+++ home_parsing_update.bat
@@ -1,8 +1,8 @@
 cd \
 mkdir home-parsing-temp
 move \home-parsing\opzioni.json \home-parsing-temp
-rmdir \home-parsing /S /Q
+rmdir /S /Q \home-parsing
 cd \
 git clone https://www.github.com/edo1998/home-parsing
-move \home-parsing-temp\opzioni.json \home-parsing /Y
+move /Y \home-parsing-temp\opzioni.json \home-parsing
 rmdir \home-parsing-temp
